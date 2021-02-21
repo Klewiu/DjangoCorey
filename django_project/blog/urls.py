592b4fcf7c008d2c247b79cf3710126b88d5e33a
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-02-21 20:18:45 +0100

    Create templates sites html
    Create templates base.html and home.html and about.html.
    Add static with main.css.
    Use Bootstrap in base.html
    Add dummy data in views.py

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views # . oznacza z bierzącego katalogu importujemy views w której mamy fukncje home
 
 urlpatterns = [
-    path('', views.home, name = 'blog-home'),
+    path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
 
 ]
