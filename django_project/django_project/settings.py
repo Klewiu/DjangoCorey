a8ba51413f95adc53d69151601850bb213b9ee34
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-02-24 22:14:17 +0100

    Create users app
    Create users app with register function.

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig', # dodaje aplikacje Blog
+    'users.apps.UsersConfig', # dodaje aplikacje Users
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
