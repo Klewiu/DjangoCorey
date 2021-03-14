8c93048eb019ecd6a68886a25a48d56327fa669b
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-03-14 21:32:52 +0100

    Add post operations - delete, detail, form, update
    Create new templates: post_confirm_delete.html , post_detail.html, post_form.html. Edit home.html template
    Edit new classes in views - ListView, DetailView, UpdateView, DeleteView

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,10 +1,15 @@
 #dodajemy plik URLS do którego prowadzi główny urls z django_project
 
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views # . oznacza z bierzącego katalogu importujemy views w której mamy fukncje home
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 
 ]
