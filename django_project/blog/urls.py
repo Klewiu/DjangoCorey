462bad2ee71f28f90c545378f61bbbe3f7be6a3f
Jakub Klewicki <kuba.klewicki@gmail.com>
2021-03-18 21:46:33 +0100

    Create user_post view, Add pagination
    Add pagination in home.html, user_posts.html
    Create UserPostListView with new template
    Add links to user-posts form main site.

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,11 +1,12 @@
 #dodajemy plik URLS do którego prowadzi główny urls z django_project
 
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views # . oznacza z bierzącego katalogu importujemy views w której mamy fukncje home
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
